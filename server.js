144931979b29c0d1fbaaaeef2fa581afc1a52560
tusharramgiri <104557273+tusharramgiri@users.noreply.github.com>
2024-04-23 00:51:12 +0530

    express config, packages installed

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const colors = require('colors');
+
+//config dotenv
+dotenv.config();
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.get('/', (req,res) => {
+    res.send("<h1>Hello</h1>");
+});
+
+//port
+const PORT = 8080 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`);
+});
